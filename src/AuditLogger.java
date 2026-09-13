e63b070cb75f502b2db01fbcd58a0766b032592e
lahiru20242082 <lahiru.20242082@iit.ac.lk>
2026-09-13 23:01:56 +0530

    Added simple file logging for audit

--- src/AuditLogger.java
+++ src/AuditLogger.java
@@ -1,7 +1,20 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class AuditLogger {
     private String fileName;
 
     public AuditLogger(String fileName) {
         this.fileName = fileName;
+    }
+
+    public void log(String action) {
+        try {
+            FileWriter writer = new FileWriter(fileName, true);
+            writer.write(action + "\n");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
