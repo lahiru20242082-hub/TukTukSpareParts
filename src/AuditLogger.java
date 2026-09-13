fb179ee58b615f62548800d10f0306036c1ca169
lahiru20242082 <lahiru.20242082@iit.ac.lk>
2026-09-13 23:02:45 +0530

    Added timestamp to audit logs

--- src/AuditLogger.java
+++ src/AuditLogger.java
@@ -1,5 +1,6 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class AuditLogger {
     private String fileName;
@@ -11,7 +12,8 @@ public class AuditLogger {
     public void log(String action) {
         try {
             FileWriter writer = new FileWriter(fileName, true);
-            writer.write(action + "\n");
+            String time = LocalDateTime.now().toString();
+            writer.write(time + " - " + action + "\n");
             writer.close();
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
